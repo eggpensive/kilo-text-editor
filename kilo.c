a788f2d854d4aa86b4c6d3ea6ab2f1527a454fea
bryan <238042797+eggpensive@users.noreply.github.com>
2026-05-07 23:55:44 +0800

    Add basic input and turn off echoing

--- kilo.c
+++ kilo.c
@@ -1,9 +1,25 @@
 #include <unistd.h>
-#include <stdio.h>
+#include <termios.h>
+
+void enableRawMode()
+{
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &raw);
+    
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDERR_FILENO, TCSAFLUSH, &raw);
+}
 
 int main()
 {
+    enableRawMode();
     
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    
+
 
     return 0;
 }
