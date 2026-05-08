1510f31c3ea557a940ab7749b4c63f193eae8133
bryan <238042797+eggpensive@users.noreply.github.com>
2026-05-09 02:13:20 +0800

    Turn off signals (ISIG) and add display keypress

--- kilo.c
+++ kilo.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 
 struct termios orig_termios;
 
@@ -15,7 +17,7 @@ void enableRawMode()
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -25,7 +27,19 @@ int main()
     enableRawMode();
     
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    {
+        if (iscntrl(c))
+        {
+            printf("%d\n", c);
+        }
+        else
+        {
+            printf("%d ('%c')\n", c, c);
+        }
+        
+        
+    }
     
 
 
